483ef2b1dc2aef8c582df2e10c26a0cb52da3e96
= <=>
2019-04-10 09:39:17 +0200

    - added some minor improvements - added some (unnecessary...) error checks

--- src/components/k8s/pcn_k8s/networkpolicies/pcn_firewall/doc.go
+++ src/components/k8s/pcn_k8s/networkpolicies/pcn_firewall/doc.go
@@ -3,4 +3,8 @@ package pcnfirewall
 const (
 	// FWM is the short name of a Firewall Manager
 	FWM = "Firewall Manager"
+	// FirstIngressID is the very first ingress ID
+	FirstIngressID = int32(1)
+	// FirstEgressID is the very first ingress ID
+	FirstEgressID = int32(1)
 )
